3fcc9bd27f38cc0939fb280b2ecf089f7bba66f2
Mathieu Lorber <mathieu.lorber@gmail.com>
2025-07-13 18:09:46 +0200

    SAVEPOINT

--- src/test/kotlin/kt2ts/resolve/FileOpenPerfTest.kt
+++ src/test/kotlin/kt2ts/resolve/FileOpenPerfTest.kt
@@ -14,7 +14,22 @@ class FileOpenPerfTest {
     @Test
     fun `test perfs`() {
         val gitDir = Path(System.getenv("HOME")).resolve("git")
-        time("coroutines") {
+        time("No coroutine") {
+            KotlinSourceFilesResolver.sequenceKotlinFiles(gitDir).forEach { file ->
+                file.readBytes()
+            }
+        }
+        time("1 coroutine") {
+            runBlocking {
+                withContext(Dispatchers.IO) {
+                    val readSemaphore = Semaphore(permits = 1)
+                    KotlinSourceFilesResolver.sequenceKotlinFiles(gitDir).forEach { file ->
+                        launch { readSemaphore.withPermit { file.readBytes() } }
+                    }
+                }
+            }
+        }
+        time("4 coroutines") {
             runBlocking {
                 withContext(Dispatchers.IO) {
                     val readSemaphore = Semaphore(permits = 4)
@@ -24,10 +39,36 @@ class FileOpenPerfTest {
                 }
             }
         }
+        time("10 coroutines") {
+            runBlocking {
+                withContext(Dispatchers.IO) {
+                    val readSemaphore = Semaphore(permits = 10)
+                    KotlinSourceFilesResolver.sequenceKotlinFiles(gitDir).forEach { file ->
+                        launch { readSemaphore.withPermit { file.readBytes() } }
+                    }
+                }
+            }
+        }
+        time("20 coroutines") {
+            runBlocking {
+                withContext(Dispatchers.IO) {
+                    val readSemaphore = Semaphore(permits = 20)
+                    KotlinSourceFilesResolver.sequenceKotlinFiles(gitDir).forEach { file ->
+                        launch { readSemaphore.withPermit { file.readBytes() } }
+                    }
+                }
+            }
+        }
+        time("No coroutine") {
+            KotlinSourceFilesResolver.sequenceKotlinFiles(gitDir).forEach { file ->
+                file.readBytes()
+            }
+        }
     }
 
     fun time(m: String, run: () -> Any) {
         val start = System.currentTimeMillis()
+        run()
         println("${m.padEnd(42)} ${(System.currentTimeMillis() - start).toString().padStart(7)}ms")
     }
 }
